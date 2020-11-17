7783ad2ab54bf7e3dfefb6aacec4994e45be7519
oluwabukola <daniellabaiyewu@gmail.com>
2020-11-17 22:35:16 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,57 @@
+const button = document.querySelector('button');
+const countries = document.querySelector('.countries');
+const submitBtn = document.querySelector('.submit-btn');
+const table = document.querySelector('tbody');
+
+
+
+button.addEventListener('click', (e) => {
+    e.preventDefault();
+    countries.style.display = "block";
+    submitBtn.style.display = "none";
+    
+})
+const response = fetch('https://restcountries.eu/rest/v2/all', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'text/plain;charset=UTF-8',
+                },
+                
+            })
+                .then(response => {
+                  return  response.json();
+                    
+                })
+                .then(data => {
+                    console.log(data);
+                    data.map(cont => {
+                        console.log(cont.name);
+                        countries.innerHTML = `
+                        <table>
+                        <thead>
+                         <tr>
+                        <th>Name</th>
+                                     <th>Capital</th>
+                                    <th>Region</th>
+                                    <th>Population</th>
+                                    <th>Flag</th>
+                                    </tr>
+    
+                                </thead>
+                                    <tbody>
+                                <tr >
+                                <td>${cont.name}</td>
+                                <td>${cont.capital}</td>
+                                <td>${cont.region}</td>
+                                <td>${cont.population}</td>
+                                <td ><img src=${cont.flag}></td>
+                                 </tr>
+                                 </tbody>
+                                 </table>`
+                    
+                    })
+                    })
+                
+                .catch((error) => {
+                    console.error('Error:', error);
+                  });
